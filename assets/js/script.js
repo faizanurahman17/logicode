1579fb2b5cc9c890a228d91430ab86296c3c7966
Faizanur Rahman <faizanurahman1711@gmail.com>
2025-05-13 11:00:09 +0530

    fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 const body = document.querySelector('body');
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
+const paras = document.querySelectorAll('p');
 const img = document.querySelector('img');
+const links = document.querySelectorAll('nav a');
+const nav = document.querySelector('nav');
+const wrapper = document.querySelector('.wrapper');
+const contants = document.querySelectorAll('.contants');
+const contantLists = document.querySelectorAll('.contants a');
+const toggle = document.querySelector('.toggle');
 
-body.addEventListener('click', colorChange);
+
+
+toggle.addEventListener('click', colorChange);
 
 let onbtn = false;
 function colorChange() {
@@ -11,12 +19,46 @@ function colorChange() {
     if (onbtn) {
         img.style.border = "2.5px black solid";
         h1.style.color = "black";
-        p.style.color = "black";
         body.style.background = "whitesmoke";
+        nav.style.borderBottom = "1.5px #c8c8c8 solid";
+        nav.style.background = "whitesmoke";
+        wrapper.style.borderBottom = "1.5px #c8c8c8 solid";
+
+        contantLists.forEach(a => {
+            a.style.background = "black";
+            a.style.color = "whitesmoke";
+        });
+        contants.forEach(cont => {
+            cont.style.border = "1px black solid";
+        });
+        paras.forEach(p =>{
+            p.style.color = "black";
+        });
+        links.forEach(link => {
+            link.style.color = "black";
+            link.style.borderBottom = "none";
+        });
     } else {
         img.style.border = "";
         h1.style.color = "";
-        p.style.color = "";
         body.style.background = "";
+        nav.style.borderBottom = "";
+        nav.style.background = "";
+        wrapper.style.borderBottom = "";
+
+        contantLists.forEach(a => {
+            a.style.background = "";
+            a.style.color = "";
+        });
+        contants.forEach(cont => {
+            cont.style.border = "";
+        });
+        paras.forEach(p =>{
+            p.style.color = "";
+        });
+        links.forEach(link => {
+            link.style.color = "";
+            link.style.borderBottom = "";
+        });
     }
 }
